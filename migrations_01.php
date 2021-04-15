ee808cb4012fb3264c5419106a139c660e121308
titus njiru <njirutitus@gmail.com>
2021-04-16 01:42:56 +0300

    Migrations

--- migrations_01.php
+++ migrations_01.php
@@ -3,6 +3,14 @@
     require_once __DIR__ . '/vendor/autoload.php';
     require_once './controllers/conn.php';
     //users table
+    $sql = "DELETE FROM song";
+    $STH = $DBH->prepare($sql);
+    $STH->execute();
+
+    $sql = "DELETE FROM album";
+    $STH = $DBH->prepare($sql);
+    $STH->execute();
+
     $sql = "DELETE FROM user;ALTER TABLE user DROP email;ALTER TABLE user ADD email tinytext unique;";
     $STH = $DBH->prepare($sql);
     $STH->execute();
