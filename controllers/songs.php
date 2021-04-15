b7558c63e364c5c6f0b47c591e24ed6fbd2619b8
titus njiru <njirutitus@gmail.com>
2021-04-16 00:21:12 +0300

    adding songs view and controller

--- controllers/songs.php
+++ controllers/songs.php
@@ -1,28 +1,51 @@
-<?php 
-    require_once './controllers/conn.php';
-    
-    try {
-        $STH = $DBH->prepare("SELECT * FROM song WHERE album=?");
-        $STH->execute([$id]);
-        while($songs = $STH->fetch()){
-            ?>
-            <tr>
-                <td><?php echo $songs['song_title']; ?></td>
-                <td><a target="_blank" href="">
-                        <button type="button" class="btn btn-success btn-xs">
-                        <i class="fas fa-play"></i>&nbsp; Play
-                        </button>
-                    </a><?php echo $songs['song_title']; ?>
-                </td>
-                <td></td>
-                <td></td>
-            </tr>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">Song Title</th>
+        <th scope="col">Artist</th>
+        <th scope="col">Audio File</th>
+        <th scope="col">Album</th>
+        <th scope="col">Favorite</th>
 
+    </tr>
+    </thead>
+    <tbody>
+
+<?php
+require_once './controllers/conn.php';
+
+try {
+    $STH = $DBH->prepare("SELECT album.album_logo as album_logo, album.id as album_id,album.album_name as album_name, album.artist as artist,song.audio_file as audio_file,song.song_title as song_title, song.is_favorite as is_favorite FROM album,song WHERE album.id = song.album and album.user=?");
+    $STH->execute([$_SESSION['user']]);
+    while($songs = $STH->fetch()){
+        ?>
+        <tr>
+            <td><?php echo $songs['song_title']; ?></td>
+            <td><?php echo $songs['artist']; ?></td>
+            <td><audio controls><source src="./media/<?php echo $songs['audio_file'] ?>"></audio></td>
+            <td><img class="img-thumbnail" style="width: 30px; float: left; margin-right: 10px;" src="./media/<?php echo $songs['album_logo']; ?>" alt="<?php echo $songs['album_name'].'-'.$songs['album_id']; ?>" /><?php echo $songs['album_name']; ?></td>
+            <td>
+                <?php
+                if ($songs['is_favorite'] == 0) {
+                    ?>
+                    <i class="far fa-star ms-2"></i>
                     <?php
-                        }
+                }
+                else {
+                    ?>
+                    <i class="fas fa-star ms-2"></i>
+                <?php }?>
+
+            </td>
+        </tr>
+
+        <?php
+    }
 }
 catch(PDOException $e){
     $_SESSION['error'] = "Couldn't fetch the songs";
     file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND); # log errors to afile
 }
 ?>
+    </tbody>
+</table>
--- songs.php
+++ songs.php
@@ -2,43 +2,15 @@
 require_once './includes/authheader.php';
 require_once './controllers/conn.php';
 ?>
-<div class="container">
-<div class="row">
-        <div class="col-sm-3 p-3 mt-2">
-            <div id="alert">
-                <?php require_once "./feedback.php"; ?>
-            </div>
 
-            <?php require_once "./controllers/album.php"; ?>
-            
-        </div>
-        <div class="col-sm-8 me-1 p-3 mt-2">
-        <h3>All Songs</h3>
-        <div class="panel panel-default">
-                <div class="panel-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Audio File</th>
-                            <th scope="col">Favorite</th>
-                            <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php require_once "./controllers/songs.php"; ?>
-                        </tbody>
-                    </table>
-                </div>
-        </div>
-        
+<div class="container">
+    <div class="row mt-3">
+        <h2>Songs</h2>
+        <div class="card shadow">
+            <div class="card-body">
+                <?php require_once "./controllers/songs.php";?>
+            </div>
         </div>
     </div>
 </div>
-
-
-<?php
- require_once './includes/footer.php';
-
- ?>
-
+<?php require_once "./includes/footer.php"; ?>
