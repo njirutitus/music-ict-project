a8504cec89c67d86ecbf0a8b0738d74919e9886b
titus njiru <njirutitus@gmail.com>
2021-04-16 01:08:04 +0300

    adding cards to contact login and register

--- login.php
+++ login.php
@@ -4,24 +4,28 @@ require_once './controllers/conn.php';
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-sm-6 m-auto p-3 border mt-2 shadow">
-            <h3 class="text-center">Log in to continue</h3>
-            <?php require_once "./controllers/login.php" ?>
+        <div class="col-sm-6 m-auto p-3 mt-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3 class="text-center">Log in to continue</h3>
+                    <?php require_once "./controllers/login.php" ?>
 
-            <div id="alert">
-                <?php require_once "./feedback.php"; ?>
+                    <div id="alert">
+                        <?php require_once "./feedback.php"; ?>
+                    </div>
+                    <form action="" method="post">
+                        <div class="from-group mb-3">
+                            <label for="email">Email</label>
+                            <input type="email" value="<?php echo $email ?? ""; ?>" id="email" name="email" class="form-control">
+                        </div>
+                        <div class="from-group mb-3">
+                            <label for="password">Password</label>
+                            <input type="password" value="<?php echo $password ?? ""; ?>" id="password" name="password" class="form-control">
+                        </div>
+                        <input type="submit" value="Submit" class="btn btn-primary" name="submit">
+                    </form>
+                </div>
             </div>
-            <form action="" method="post">
-                <div class="from-group mb-3">
-                    <label for="email">Email</label>
-                    <input type="email" value="<?php echo $email ?? ""; ?>" id="email" name="email" class="form-control">
-                </div>
-                <div class="from-group mb-3">
-                    <label for="password">Password</label>
-                    <input type="password" value="<?php echo $password ?? ""; ?>" id="password" name="password" class="form-control">
-                </div>
-                <input type="submit" value="Submit" class="btn btn-primary" name="submit">
-            </form>
         </div>
     </div>
 </div>
