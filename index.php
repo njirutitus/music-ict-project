0347b0488c7faf1e6f3a5d6407c2d4b3fa6b5c12
titus njiru <njirutitus@gmail.com>
2021-07-23 18:02:14 +0300

    updating hero section

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-    <?php
-
+<?php
 require_once './includes/header.php';
 
 ?>
 
 
 <section class="container" style="min-height: 100%">
-<div class="hero">
-<div class="hero-text">
-    <h3>Welcome to Viberr</h3>
-    <p>Viberr is an application that let's you upload, store, and play all of your music from the cloud. You can now manage and listen to your music from any device, anywhere in the world.</p>
-</div>
-    </section>
+    <div class="hero">
+        <div class="hero-text">
+            <h3>Welcome to Viberr</h3>
+            <p>Viberr is an application that let's you upload, store, and play all of your music from the cloud. You can
+                now manage and listen to your music from any device, anywhere in the world.</p>
+        </div>
+</section>
 </div>
 
 
